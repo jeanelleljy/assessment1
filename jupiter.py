c5656450ae7613f1e0ac44c0480d90cff09e1c5a
Jeanelle <fz22667@bristol.ac.uk>
2024-01-24 01:37:18 +0000

    Task 2 finished

--- jupiter.py
+++ jupiter.py
@@ -1,6 +1,10 @@
+# TASK 1
 import pandas as pd
 import sqlite3
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+import numpy as np
 
 data = 'jupiter.db'  # store the full dataset as "data"
 
@@ -28,3 +32,32 @@ class Moons:
 
     def __del__(self):
         self.conn.close()
+
+# TASK 2
+
+    def add_columns(self):  
+        self.data['T2'] = (self.data['period_days']*60*60*24)**2  # Add new column T2 (converted to seconds)
+        self.data['a3'] = (self.data['distance_km']*1000)**3  # Add new column a3 (converted to metres)
+        
+    def train_test_split(self):
+        ## Create a testing and a training dataset
+        # Prepare the input data, separating into training and testing sets
+        X = self.data[['a3']]
+        Y = self.data['T2']
+        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(X, Y, random_state=42)
+        
+    def linear_regression_model(self):
+        self.model = linear_model.LinearRegression(fit_intercept=True)  # Create an instance of the model
+        self.model.fit(self.X_train, self.Y_train)  # Train the model using the training set
+
+    def estimate_jupiter_mass(self, G=6.67e-11):
+        # Kepler's law: T^2 = (4 * pi^2 * a^3) / (G * M)
+        # M = (4 * pi^2 * a^3) / (G * T^2)
+        # coef = (4 * pi^2) / (G * M)
+        # Hence M = (4 * pi^2) / (G * coef)        
+        coef = self.model.coef_[0]
+        jupiter_mass = (4 * np.pi**2) / (G * coef)
+        return jupiter_mass
+    
+    def model_score(self):
+        return self.model.score(self.X_test, self.Y_test)  # Returns the R2 score for this model
